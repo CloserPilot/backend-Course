30f58cec6ba2ff268a039def208a6ee8ab83e5d2
closerpilot <pavonheredia@hotmail.com>
2026-01-29 18:24:32 -0600

    Inicio del backend

---
+++ backend/src/config/database.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = async (uri) => {
+    try {
+        const conectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);   
+        console.log(`\n MongoDB connected!!!
+            ${conectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
