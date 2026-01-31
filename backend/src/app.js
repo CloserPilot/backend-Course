da3d80e5f0da369f8cbcc4e79971f5f2f7b64377
closerpilot <pavonheredia@hotmail.com>
2026-01-31 13:41:30 -0600

    add CRUD API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import userRouter from './routes/user.route.js';
 
 
 const app = express(); //crete an express app
 app.use(express.json());
 
+
+import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.routes.js';
 // routes declaration
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/posts', postRouter);
 
 //example route http://localhost:4000/api/v1/users/register
 
